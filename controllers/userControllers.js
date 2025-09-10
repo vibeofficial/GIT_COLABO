e1901a7bec49d72655f6322ab6f1fe8666f2c7fc
aninkud2 <adetadeservices@gmail.com>
2025-09-10 16:02:22 +0100

    no req body

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,72 +1,65 @@
+const userModel = require("../models/user");
 
-const usermodel = require("../models/user")
+exports.createUser = async (req, res) => {
+  try {
+    const { firstName, age, gender, email } = req.body;
+    const data = {
+      firstName,
+      age,
+      gender,
+      email,
+    };
 
-
-exports.createUser=async(req, res)=>{
-    try {
-        const {firstName,age,gender,email}= req.body
-        const data ={
-            firstName,
-            age,
-            gender,
-            email
-
-        }
-const createdUser=await usermodel.create(data)
-res.status(201).json(
-    {
-        meesage:"user Created",
-        data:createdUser
-    }
-)
-
-    } catch (error) {
-        res.status(500).json({
-            message:"internal server error",
-            error:error.message
-        })
-    }
-
-}
-
-
-
-const userModel = require('../models/user');
-
+    const createdUser = await userModel.create(data);
+    res.status(201).json({
+      message: "User created",
+      data: createdUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
 exports.getAll = async (req, res) => {
   try {
     const users = await userModel.find();
-    
+
     if (users.length === 0) {
-      return res.status(404).json('No user found')
-    };
+      return res.status(404).json({ message: "No user found" });
+    }
 
     res.status(200).json({
-      message: 'All users below',
-      data: users
-    })
+      message: "All users below",
+      data: users,
+    });
   } catch (error) {
-    res.status(500).json('Error getting all users', error.message)
+    res.status(500).json({
+      message: "Error getting all users",
+      error: error.message,
+    });
   }
 };
 
 exports.getOne = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await userModel.findById(id);
-    
+
     if (!user) {
-      return res.status(404).json('User not found')
-    };
+      return res.status(404).json({ message: "User not found" });
+    }
 
     res.status(200).json({
-      message: 'User below',
-      data: user
-    })
+      message: "User below",
+      data: user,
+    });
   } catch (error) {
-    res.status(500).json('Error getting all users', error.message)
-
-
+    res.status(500).json({
+      message: "Error getting user",
+      error: error.message,
+    });
+  }
 };
-}
