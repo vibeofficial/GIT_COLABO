de913ceed85f0ed8151c822e64d6f36e1cf9e30b
aninkud2 <adetadeservices@gmail.com>
2025-09-10 18:57:15 +0100

    validate

--- server.js
+++ server.js
@@ -4,17 +4,11 @@ const mongoose = require('mongoose');
 const { getAll, getOne, deleteUser, createUser } = require('./controllers/userControllers');
 const PORT = process.env.PORT || 1234;
 const DB = process.env.DB_URI
-
 const app = express();
-
 app.use(express.json());
 app.post('/create', createUser)
 app.get('users', getAll);
 app.get('/user/:id', getOne);
-
-
-app.delete('/user/:id', deleteUser);
-
 mongoose.connect(DB).then(()=>{
   console.log('Connected to Database'),
   app.listen(PORT, ()=>{
