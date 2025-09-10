0503216aa93aa60dc63163798afc5c487b42b4b9
vibeofficial <ichioguchristoper98@gmail.com>
2025-09-10 15:15:46 +0100

    updated

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { getAll, getOne, deleteUser } = require('./controllers/userControllers');
+const { getAll, getOne, deleteUser, createUser } = require('./controllers/userControllers');
 const PORT = process.env.PORT || 1234;
 const DB = process.env.DB_URI
-const createdUser = require("./controllers/userControllers")
+// const createdUser = require("./controllers/userControllers")
 const app = express();
 
 app.use(express.json());
+app.post('/create', createUser)
 app.get('users', getAll);
 app.get('/user/:id', getOne);
 app.delete('/user/:id', deleteUser);
