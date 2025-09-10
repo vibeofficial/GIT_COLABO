076c23cf169a46249edbdef59f70dce14e427de7
Aninku Joshua <adetadeservices@gmail.com>
2025-09-10 19:39:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.post('/create', createUser)
 app.get('user', getAll);
 
 app.get('/user/:id', getOne);
-app.delete("/user/:id",deleteUser)
+app.delete("/user/:id",deleteUser)// testing something by conflict
 
 app.get('/user/:id', getOne);
 
